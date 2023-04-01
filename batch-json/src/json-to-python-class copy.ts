0859ddd1ce83acdf51b71cb1e8d2587ac51db410
wangwei <434533508@qq.com>
2023-04-01 11:38:13 +0800

    fix: portal

--- batch-json/src/json-to-python-class copy.ts
+++
@@ -1,8 +0,0 @@
-import register from './utils/register';
-
-export default register({
-  inputHandler: async (str: string) => {
-    return await window.$he3.jsonToOther('python', str);
-  },
-  lang: 'PYTHON',
-});
