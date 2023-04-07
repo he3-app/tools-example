252a4a488baf8a90e975b3d7b37cc687e73073ce
Mashirowww <1204843079@qq.com>
2023-04-07 16:40:54 +0800

    fix: fix precision

--- batch-json/src/json-to-pretty.ts
+++ batch-json/src/json-to-pretty.ts
@@ -1,43 +1,74 @@
-import prettyJson from 'json-stringify-pretty-compact';
-import Decimal from 'decimal.js';
 import { uglyJsonExample } from './example';
 import register from './utils/register';
 
-//将数字转换成高精度类型
-const toSafeJSON = (key, value) => {
-  if (typeof value === 'number') {
-    const decimalValue = new Decimal(value);
-    return decimalValue.toString();
+function formatJson(jsonString: string, indent: number = 2): string {
+  let result = '';
+  let indentLevel = 0;
+  let inString = false;
+  let currentChar = '';
+
+  for (let i = 0, length = jsonString.length; i < length; i++) {
+    currentChar = jsonString.charAt(i);
+
+    switch (currentChar) {
+      case '{':
+      case '[':
+        if (!inString) {
+          result += currentChar + '\n' + getIndent(++indentLevel, indent);
+        } else {
+          result += currentChar;
+        }
+        break;
+      case '}':
+      case ']':
+        if (!inString) {
+          result += '\n' + getIndent(--indentLevel, indent) + currentChar;
+        } else {
+          result += currentChar;
+        }
+        break;
+      case ',':
+        if (!inString) {
+          result += ',\n' + getIndent(indentLevel, indent);
+        } else {
+          result += currentChar;
+        }
+        break;
+      case ':':
+        if (!inString) {
+          result += ': ';
+        } else {
+          result += currentChar;
+        }
+        break;
+      case ' ':
+      case '\n':
+      case '\t':
+        if (inString) {
+          result += currentChar;
+        }
+        break;
+      case '"':
+        inString = !inString;
+        result += currentChar;
+        break;
+      default:
+        result += currentChar;
+        break;
+    }
   }
-  return value;
+
+  return result;
 }
-//将json分段解析 提高性能
-function* chunkify(str, size) {
-  const chunks = Math.ceil(str.length / size);
-  for (let i = 0, index = 0; i < chunks; ++i, index += size) {
-    yield str.substr(index, size);
-  }
+
+function getIndent(indentLevel: number, indentSize: number): string {
+  return ' '.repeat(indentLevel * indentSize);
 }
+
+
 
 export default register({
-  inputHandler: (str: string) => {
-    const chunkSize = 10000;
-    const stream = chunkify(str, chunkSize);
-    let json = '';
-
-    for (const chunk of stream) {
-      const parsedChunk = JSON.parse(chunk, (key, value) => {
-        if (typeof value === 'string' && /^\d+$/.test(value)) {
-          const decimalValue = new Decimal(value);
-          return decimalValue.toNumber();
-        }
-        return value;
-      });
-      json += prettyJson(parsedChunk, { maxLength: 20, replacer: toSafeJSON });
-    }
-
-    return json;
-  },
+  inputHandler: formatJson,
   lang: 'JSON',
   example: uglyJsonExample,
 });
