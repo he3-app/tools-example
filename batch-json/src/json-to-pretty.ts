94cc80406b07a3f73453d454854c31747da735f7
Mashirowww <1204843079@qq.com>
2023-04-07 14:35:53 +0800

    fix: json pretty

--- batch-json/src/json-to-pretty.ts
+++ batch-json/src/json-to-pretty.ts
@@ -1,11 +1,27 @@
 import prettyJson from 'json-stringify-pretty-compact';
+import Decimal from 'decimal.js';
 import { uglyJsonExample } from './example';
 import register from './utils/register';
 
+const toSafeJSON = (key, value) => {
+  if (typeof value === 'number') {
+    const decimalValue = new Decimal(value);
+    return decimalValue.toString();
+  }
+  return value;
+}
+
 export default register({
   inputHandler: (str: string) => {
-    const json = JSON.parse(str);
-    return prettyJson(json, { maxLength: 20 });
+    const json = JSON.parse(str, (key, value) => {
+      // 如果 value 是字符串类型并且可以转换为数字，则使用 Decimal 对象存储
+      if (typeof value === 'string' && /^\d+$/.test(value)) {
+        const decimalValue = new Decimal(value);
+        return decimalValue.toNumber();
+      }
+      return value;
+    });
+    return prettyJson(json, { maxLength: 20, replacer: toSafeJSON });
   },
   lang: 'JSON',
   example: uglyJsonExample,
