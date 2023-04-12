cc3041dd601a831608b8e9608fc54ea3ab01386f
tanghengtao <2121065239@qq.com>
2023-04-12 15:45:34 +0800

    fix: timestamp-format, regex-tester,batch-json

--- timestamp-format/src/index.vue
+++ timestamp-format/src/index.vue
@@ -12,9 +12,44 @@
             <h-select v-model:value="timestampFormat" @change="formatTimestamp">
               <a-select-option title="s" value="s" />
               <a-select-option title="ms" value="ms" />
+              <a-select-option title="us" value="us" />
+              <a-select-option title="ns" value="ns" />
             </h-select>
           </template>
         </h-number-input>
+      </a-form-item>
+
+      <a-form-item v-show="!props.renderOnSearch" :label="t('inputTimezone')">
+        <a-select
+          v-model:value="inputTimezone"
+          show-search
+          @change="formatTimestamp"
+        >
+          <a-select-option
+            v-for="item in timezoneList"
+            :key="item.tzCode"
+            :title="item.tzCode"
+            :value="item.tzCode"
+            show-search
+          />
+        </a-select>
+      </a-form-item>
+      <a-form-item v-show="!props.renderOnSearch" :label="t('outputTimezone')">
+        <a-select
+          v-model:value="outputTimezone"
+          show-search
+          @change="formatTimestamp"
+        >
+          <a-select-option
+            v-for="item in timezoneList"
+            :key="item.tzCode"
+            :title="item.tzCode"
+            :value="item.tzCode"
+          />
+        </a-select>
+      </a-form-item>
+      <a-form-item v-show="!props.renderOnSearch" :label="t('enableUTC')">
+        <a-switch v-model:checked="enableUtc" @change="formatTimestamp" />
       </a-form-item>
     </a-form>
 
@@ -23,14 +58,14 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, onMounted, ref } from 'vue';
-import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
-import { useI18n } from 'vue-i18n';
-import timezonesData from '../data/timezones.json';
-import { DateFormatEnum } from './enums';
-import messages from './lang.json';
+import { defineProps, onMounted, ref } from "vue";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import { useI18n } from "vue-i18n";
+import timezonesData from "../data/timezones.json";
+import { DateFormatEnum } from "./enums";
+import messages from "./lang.json";
 
 const { t } = useI18n({
   locale: window.$he3.lang,
@@ -39,7 +74,7 @@ const { t } = useI18n({
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-// sort data by tzCode
+
 const timezoneList = timezonesData.sort((a, b) => {
   return a.tzCode.localeCompare(b.tzCode);
 });
@@ -47,7 +82,7 @@ const timezoneList = timezonesData.sort((a, b) => {
 const props = defineProps({
   defaultTimestampFormat: {
     type: String,
-    default: 's',
+    default: "s",
   },
   enableUTC: {
     type: Boolean,
@@ -74,14 +109,32 @@ onMounted(() => {
   $he3.getLastClipboard().then((res) => {
     if (isNumber(res)) {
       const clipboardValue = Number.parseInt(res);
-      const maxDate = dayjs('2080-12-31 23:59:59', 'YYYY-MM-DD HH:mm:ss');
-      const minDate = dayjs('1980-01-01 00:00:00', 'YYYY-MM-DD HH:mm:ss');
-      if (clipboardValue >= minDate.unix() && clipboardValue <= maxDate.unix()) {
+      const maxDate = dayjs("2080-12-31 23:59:59", "YYYY-MM-DD HH:mm:ss");
+      const minDate = dayjs("1980-01-01 00:00:00", "YYYY-MM-DD HH:mm:ss");
+      if (
+        clipboardValue >= minDate.unix() &&
+        clipboardValue <= maxDate.unix()
+      ) {
         timestamp.value = clipboardValue;
-        timestampFormat.value = 's';
-      } else if (clipboardValue >= minDate.valueOf() && clipboardValue <= maxDate.valueOf()) {
+        timestampFormat.value = "s";
+      } else if (
+        clipboardValue >= minDate.valueOf() &&
+        clipboardValue <= maxDate.valueOf()
+      ) {
         timestamp.value = clipboardValue;
-        timestampFormat.value = 'ms';
+        timestampFormat.value = "ms";
+      } else if (
+        clipboardValue >= minDate.valueOf() * 1000 &&
+        clipboardValue <= maxDate.valueOf() * 1000
+      ) {
+        timestamp.value = clipboardValue;
+        timestampFormat.value = "us";
+      } else if (
+        clipboardValue >= minDate.valueOf() * 1000000 &&
+        clipboardValue <= maxDate.valueOf() * 1000000
+      ) {
+        timestamp.value = clipboardValue;
+        timestampFormat.value = "ns";
       }
     }
     formatTimestamp();
@@ -100,11 +153,11 @@ const formatOptions: { label: string; value: string }[] = [];
 if (props.renderOnSearch) {
   formatOptions.push(
     {
-      label: 'RFC2822',
+      label: "RFC2822",
       value: DateFormatEnum.RFC2822,
     },
     {
-      label: 'ISO8601',
+      label: "ISO8601",
       value: DateFormatEnum.ISO8601,
     }
   );
@@ -124,8 +177,12 @@ function formatTimestamp() {
   }
 
   let formatTimestamp = dayjs(timestamp.value).tz(inputTimezone.value);
-  if (timestampFormat.value === 's') {
+  if (timestampFormat.value === "s") {
     formatTimestamp = dayjs.unix(timestamp.value).tz(inputTimezone.value);
+  } else if (timestampFormat.value === "us") {
+    formatTimestamp = dayjs(timestamp.value / 1000).tz(inputTimezone.value);
+  } else if (timestampFormat.value === "ns") {
+    formatTimestamp = dayjs(timestamp.value / 1000000).tz(inputTimezone.value);
   }
 
   let outputFormatTimestamp = dayjs.tz(formatTimestamp, outputTimezone.value);
