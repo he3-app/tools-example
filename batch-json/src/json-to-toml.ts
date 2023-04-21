65745df1739de4a19615d00a89f3f8b2a6f7f983
congcongnalian <2121065239@qq.com>
2023-04-21 16:43:13 +0800

    fix: make it work

--- batch-json/src/json-to-toml.ts
+++ batch-json/src/json-to-toml.ts
@@ -1,7 +1,9 @@
 import { jsonToToml } from './utils/json2Toml';
+import {tomlToJson} from './utils/json2Toml'
 import register from './utils/register';
 
 export default register({
   inputHandler: jsonToToml,
+  resultHandler: tomlToJson,
   lang: 'TOML',
 });
