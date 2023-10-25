50abbd5562e6f34d6b436238703778bb409564a3
yinjg <yinjg1997@163.com>
2023-10-25 16:02:16 +0800

    fix: fix load-caculator

--- loan-calculator/src/index.vue
+++ loan-calculator/src/index.vue
@@ -1,4 +1,5 @@
 <template>
+  {{ interestRate }}
   <h-single-layout mode="middle" :width="800">
     <a-form v-show="!he3.renderOnSearch" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" style="margin-left: 10%;">  
       <a-form-item :label="t('LoanPrincipal')">
@@ -136,7 +137,9 @@ const he3 = window.$he3;
 const loanAmount = ref<number>(1000000);
 const latestLPR = ref<number>(4.3);
 const LPRPlusValue = ref<number>(0);
+// 年利率
 const interestRate = ref<number>(0);
+// 贷款期限
 const loanTerm = ref<number>(360);
 const repaymentType = ref<number>(0);
 var monthlyPaymentDetail = ref<number[][]>([])
@@ -144,10 +147,8 @@ var totalPayNum = ref<number>(0);
 var totalInterestNum = ref<number>(0);
 
 const changeMonth = function(e){
-  // console.log(e);
-  // debugger;
   loanTerm.value = e;
-} 
+}
 
 function calculateInterestRate() {
   interestRate.value = Number(latestLPR.value) + 0.01*Number(LPRPlusValue.value);
@@ -155,6 +156,7 @@ function calculateInterestRate() {
 
 function monthlyInterestRate() {
   // 计算月利率
+  if (interestRate.value === 0) return 0
   return interestRate.value / 12 / 100;
 }
 
@@ -162,16 +164,18 @@ function monthlyPayment() {
   let res: number[][] = []
   // 计算每月还款额
   if (repaymentType.value==0) {
-    if (loanAmount.value === 0 || interestRate.value === 0 || loanTerm.value === 0) {
-      console.log('Returning 0')
-      // res.push(ref<number>(0).value);
+    if (loanAmount.value === 0 || loanTerm.value === 0) {
       return res;
     }
-    console.log('Not returning 0')
     const n = loanTerm.value; // 总期数
     const mir = monthlyInterestRate(); // 月利率
     const pow = Math.pow(1 + mir, n); // (1+i)^n
-    const monthlyPaymentAmount = (loanAmount.value * mir * pow) / (pow - 1)
+    let monthlyPaymentAmount: number
+    if (mir === 0) {
+      monthlyPaymentAmount = loanAmount.value / n
+    } else {
+      monthlyPaymentAmount = (loanAmount.value * mir * pow) / (pow - 1)
+    }
     var leftLoanAmount = Number(loanAmount.value);
     for (let i=0;i<n;i++){
       leftLoanAmount -= monthlyPaymentAmount - leftLoanAmount*mir;
@@ -179,13 +183,11 @@ function monthlyPayment() {
       var temp = [monthlyPaymentAmount, leftLoanAmount*mir, monthlyPaymentAmount-leftLoanAmount*mir, leftLoanAmount]
       res.push(temp);
     }
-  } else if (repaymentType.value==1) {
-    if (loanAmount.value === 0 || interestRate.value === 0 || loanTerm.value === 0) {
-      console.log('Returning 0')
-      // res.push(ref<number>(0).value);
+  }
+  else if (repaymentType.value==1) {
+    if (loanAmount.value === 0 || loanTerm.value === 0) {
       return res;
     }
-    console.log('Not returning 0')
     const n = loanTerm.value; // 总期数
     const mir = monthlyInterestRate(); // 月利率
     var leftLoanAmount = Number(loanAmount.value);
@@ -207,7 +209,6 @@ function totalPay() {
   for (let i=0;i<monthlyPaymentList.length;i++){
     sum+=monthlyPaymentList[i][0];
   }  
-  console.log(sum)
   return sum;
 }
 
@@ -240,15 +241,12 @@ function downloadCSV(csv, filename) {
   
     document.body.appendChild(downloadLink);  
     downloadLink.click();  
-    console.log(csv);
 }
   
 
 //user-defined function to export the data to CSV file format  
 function exportTableToCSV(filename) {  
-  //declare a JavaScript variable of array type  
-  // console.log(filename);
-  var csv = [];  
+  var csv = [];
   var rows = document.querySelectorAll("table tr");  
    
   //merge the whole data in tabular form   
@@ -258,8 +256,6 @@ function exportTableToCSV(filename) {
       row.push(cols[j].innerText);
     csv.push(row.join(","));
   }
-  console.log(csv);
-  //call the function to download the CSV file  
   downloadCSV(csv.join("\n"), filename);
 }
 
@@ -270,7 +266,7 @@ function exportTableToCSV(filename) {
     table-layout: fixed;
     width: 100%;
     border-collapse: separate;
-    border: 2px solid var(--primary-color);
+    border: 2px solid var(--divider-color);
     border-spacing: 0;
     border-radius: 10px;
   }
